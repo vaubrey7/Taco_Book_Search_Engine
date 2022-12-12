5cb92e62b011cf2e49864303da57fde80d9e0916
Vaughn Aubrey <vaubrey7@gmail.com>
2022-12-11 21:59:23 -0700

    deploy

--- README.md
+++ README.md
@@ -20,7 +20,7 @@
 
 a fully functioning Google Books API search engine built with a RESTful API that was refactored to be a GraphQL API built with Apollo Server. The app was built using the MERN stack, with a React front end, MongoDB database, and Node.js/Express.js server and API. It's already set up to allow users to save book searches to the back end.
 
-Deployed live app at: 
+Deployed live app at: https://git.heroku.com/taco-book-search0-engine.git
 
 ## Table of Contents
 - [Installation](#installation)
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,17 +1,17 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
-// module.exports = mongoose.connection;
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/Users-Books',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-);
+module.exports = mongoose.connection;
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost:27017/Users-Books',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+// );
